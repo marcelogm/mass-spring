3a3090bc9ee3c5ba372fdd444bd4009a91fe8d9e
Marcelo Martins <marcelo-martins@defensoria.rs.def.br>
2023-08-23 00:51:13 -0300

    chore: apply .gitignore

--- out/build/x64-Release/lib/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/marce/UFRGS/mass-spring/lib/glm/glm
-# Build directory: C:/Users/marce/UFRGS/mass-spring/out/build/x64-Release/lib/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
